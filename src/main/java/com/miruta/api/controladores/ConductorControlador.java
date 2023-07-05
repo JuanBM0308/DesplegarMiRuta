58fea1ffb1542825aae463ee777b061c2aa26c1f
Alexis Arevalo <arevaloalexis764@gmail.com>
2023-07-05 15:58:49 -0500

    Metodo buscar conductor por su conexion de idUsu a la tabla de usuario

--- src/main/java/com/miruta/api/controladores/ConductorControlador.java
+++ src/main/java/com/miruta/api/controladores/ConductorControlador.java
@@ -1,9 +1,29 @@
 package com.miruta.api.controladores;
 
+import com.miruta.api.entidades.Conductor;
+import com.miruta.api.servicios.ConductorServicioImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/conductor")
 public class ConductorControlador {
+
+    //Objeto para servicio de conductor
+    @Autowired
+    ConductorServicioImpl conductorServicio;
+
+
+
+    //Metodo para buscar conductor por idUsu
+    @GetMapping("/buscar/{idUsu}")
+    public Optional<Conductor> getConductorCon(@PathVariable("idUsu") Long idUsu) {
+        return conductorServicio.getConductor(idUsu);
+    }
+
 }
--- src/main/java/com/miruta/api/interfaces/InConductorDao.java
+++ src/main/java/com/miruta/api/interfaces/InConductorDao.java
@@ -1,7 +1,13 @@
 package com.miruta.api.interfaces;
 
 import com.miruta.api.entidades.Conductor;
+import com.miruta.api.entidades.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface InConductorDao extends JpaRepository<Conductor, Long> {
+
+    Optional<Conductor> findByUsuario(Usuario usuario);
+
 }
